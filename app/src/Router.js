ef3aa0c89224301a8b32d1cc230d721cd9f2af7e
David <Criptic@users.noreply.github.com>
2018-11-21 15:42:31 +0100

    add tabbar navigation after login

--- app/src/Router.js
+++ app/src/Router.js
@@ -1,8 +1,38 @@
 import React from "react";
 import { Scene, Router, Actions } from "react-native-router-flux";
+import { Text, View } from "react-native";
+import { Icon } from "react-native-elements";
 
 import LoginForm from "./components/LoginForm";
 import RulesOverview from "./components/RulesOverview";
+
+const IconView = ({ title, focused }) => {
+    const color = focused ? "#f44242" : "#517fa4";
+    let image = "";
+    let size = "";
+
+    switch (title) {
+        case "Rules Overview":
+            image = "home";
+            size = 36;
+            break;
+        case "Investments":
+            image = "trending-up";
+            size = 36;
+            break;
+        case "Transactions":
+            image = "inbox";
+            size = 32;
+            break;
+    }
+
+    return (
+        <View>
+            <Icon name={image} size={size} type="material" color={color} />
+            <Text>{title}</Text>
+        </View>
+    );
+};
 
 const RouterComponent = () => {
     const { navigationBarTitleStyle } = styles;
@@ -20,12 +50,33 @@ const RouterComponent = () => {
                         initial
                     />
                 </Scene>
-                <Scene key="investing" titleStyle={navigationBarTitleStyle}>
+                <Scene
+                    key="investing"
+                    titleStyle={navigationBarTitleStyle}
+                    initial
+                    tabs={true}
+                    showLabel={false}
+                >
                     <Scene
-                        key="overview"
+                        key="RulesOverview"
                         component={RulesOverview}
-                        title="Overview"
+                        icon={IconView}
                         initial
+                        title="Rules Overview"
+                    />
+
+                    <Scene
+                        key="Investments"
+                        component={RulesOverview}
+                        icon={IconView}
+                        title="Investments"
+                    />
+
+                    <Scene
+                        key="Transactions"
+                        component={RulesOverview}
+                        icon={IconView}
+                        title="Transactions"
                     />
                 </Scene>
             </Scene>
