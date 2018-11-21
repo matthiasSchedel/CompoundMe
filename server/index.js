8f242092d72b7961544bc7267b08d2792a252a93
Max Rumford <max.rumford@gmx.net>
2018-11-21 15:41:58 +0100

    pushing transformation to firebase

--- server/index.js
+++ server/index.js
@@ -20,58 +20,95 @@ var db = firebase.database();
 var app = express();
 app.use(bodyParser());
 
+/**
+ * @function
+ * @name parseCategoryCode
+ * @param {String} categoryCode VISA merchant category code
+ * @returns {String} category parsed category String
+ */
 function parseCategoryCode(categoryCode) {
   // eval transformation of categoryCode to category
+  // TODO
 
-  var category = "Fast-Food";
+  var category = "discount stores";
 
   return category;
 }
 
+/**
+ * @function
+ * @name pushTransactionToDb
+ * @param {Object} transaction transaction to be inserted into firebase
+ * @param {String} userid user to whom the transaction belongs
+ */
 function pushTransactionToDb(transaction, userid) {
   db.ref(`/users/${userid}/transactions`).push(transaction);
   // db.ref("/users/userid/transaction") .push({ descisionId, ... })
 }
 
+/**
+ * @function
+ * @name getUserByPrimaryAccoutNumber
+ * @param {string} primaryAccountNumber VISA account number to be matched to CM username
+ * @returns {string}
+ */
 function getUserByPrimaryAccoutNumber(primaryAccountNumber) {
-  // db.ref(``);
+  const username = db.ref(`/translateCardToUser/${primaryAccountNumber}`).once(
+    "value",
+    (snapshot) => {
+      return snapshot.val();
+    },
+    (errorObject) => {
+      console.error("error", errorObject);
+    }
+  );
+  return username;
 }
 
+// app listening on port 3000
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+// POST of VISA-transaction-notification
 app.post("/api/transactionDetails", (req, res, next) => {
   // extract data from incoming request body
   [...notificationDetails] = req.body.resource.notificationDetails;
   notificationPayload =
     notificationDetails[0].outBoundAlertsNotificationPayload;
 
+  transactionDetails = notificationPayload.transactionDetails;
+  // console.log(transactionDetails);
+
+  // retrieving fields from VISA-Request
+  const primaryAccountNumber = transactionDetails.primaryAccountNumber;
+  const merchantInfo = transactionDetails.merchantInfo;
+  const amount = transactionDetails.cardholderBillAmount / 100;
+
+  const merchantCategoryCode = merchantInfo.merchantCategoryCode;
+  const merchantName = merchantInfo.name;
+
+  // parse merchantCategoryCode into merchantCategory
+  const merchantCategory = parseCategoryCode(merchantCategoryCode);
+
+  // constructing transaction object to be stored in firebase
   const transaction = {
-    primaryAccountNumber:
-      notificationPayload.transactionDetails.primaryAccountNumber,
-    userIdentifier: notificationPayload.transactionDetails.userIdentifier,
-    requestReceivedTimeStamp:
-      notificationPayload.transactionDetails.requestReceivedTimeStamp,
-    merchantName: notificationPayload.transactionDetails.merchantInfo.name
+    cardNumber: primaryAccountNumber,
+    amount: amount,
+    merchantName: merchantName,
+    merchantCategory: merchantCategory,
+    timestamp: transactionDetails.requestReceivedTimeStamp,
+    userId: transactionDetails.userIdentifier
   };
-  // console.log(transaction);
+
+  // pushing transaction to db when user is identified
+  const user = getUserByPrimaryAccoutNumber(primaryAccountNumber).then(
+    (value) => {
+      username = value.val();
+      pushTransactionToDb(transaction, username);
+    }
+  );
+
+  // default response - currently no logic
   res.send(transaction);
-  // body = req.body;
-
-  // const transaction = {
-  //   cardNr: body.primaryAccountNumber,
-  //   amount: body.transactionAmount,
-  //   merchantName: body.MerchantInfo.name,
-  //   merchantCategory:
-  // };
-  // const cardNumber =
-  //   // userId to be fetched from firebase by mapping VISA-Card-number to userId
-  //   // const userId;
-  //   // db.ref("/users/"+userId)
-
-  //   (res.body = req.body);
-  // res.body.reply = {
-  //   statusCode: "200"
-  // };
-  // res.send(res.body);
 });
